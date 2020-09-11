e7b97cccff0a00fe26e750978f845b3dd23cc889
devaraujstein <andreearaujo98@gmail.com>
2020-09-11 16:53:54 -0300

    authentication

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  // Validação do token JWT
+
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('JWT token is missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  const decoded = verify(token, '');
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import AppointmentRouter from './appointments.route';
 import UsersRouter from './users.route';
+import SessionsRouter from './sessions.route';
 
 const routes = Router();
 
 routes.use('/appointments', AppointmentRouter);
 routes.use('/users', UsersRouter);
+routes.use('/sessions', SessionsRouter);
 
 export default routes;
